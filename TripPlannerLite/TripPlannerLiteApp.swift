a1e009a079ca1acb1725208d25488de81bff6f64
zoras12 <zoras@umich.edu>
2026-02-15 22:41:17 -0800

    Initial Commit

---
+++ TripPlannerLite/TripPlannerLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerLiteApp.swift
+//  TripPlannerLite
+//
+//  Created by Zora Smith on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
